21a856d4147016e54c43ccf16c1f23ff1f15f9ea
Kan Zhang <zhangkan1111@gmail.com>
2022-03-10 11:34:31 -0500

    Add RDS instance and security gourp. Modify template Userdata.

--- build/create_stack.sh
+++ build/create_stack.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 aws cloudformation create-stack --profile=dev --stack-name BasicWeb --template-body file://~/Temp/infrastructure/csye6225-infra.json \
- --parameters ParameterKey=KeyName,ParameterValue=crypto ParameterKey=ImageId,ParameterValue=ami-0ce9041f2a185676f
+ --parameters ParameterKey=KeyName,ParameterValue=crypto ParameterKey=ImageId,ParameterValue=ami-0cf0e3de90b238bcb \
+ ParameterKey=BucketName,ParameterValue=xvaefsd.dev.kanzhang.me --capabilities CAPABILITY_NAMED_IAM
---
+++ build/validate_stack.sh
@@ -0,0 +1,1 @@
+aws cloudformation validate-template --template-body file://~/Temp/infrastructure/csye6225-infra.json
