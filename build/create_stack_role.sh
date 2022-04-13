331e68c7ceca48d9d8fd0a20cd5b5586dfb9416d
Kan Zhang <zhangkan1111@gmail.com>
2022-04-13 11:54:42 -0400

    Add Lambda, SNSTopic, Policy and Permission

--- build/create_stack_role.sh
+++ build/create_stack_role.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-aws cloudformation create-stack --profile=demo --stack-name BasicRole --template-body file://~/Temp/infrastructure/IAMrole.yaml \
- --parameters  ParameterKey=WebSourceBucket,ParameterValue=codedeploy.prod.kanzhang.me \
+aws cloudformation create-stack --profile=demo --stack-name BasicRole \
+--template-body file://./IAMrole.yaml \
+ --parameters  \
+ ParameterKey=WebSourceBucket,ParameterValue=codedeploy.prod.kanzhang.me \
  ParameterKey=CodeDeployAppName,ParameterValue=csye6225-webapp \
- ParameterKey=GitHubActionIAMUser,ParameterValue=ghactions-app --capabilities CAPABILITY_NAMED_IAM
+ ParameterKey=GitHubActionIAMUser,ParameterValue=ghactions-app \
+ --capabilities CAPABILITY_NAMED_IAM
--- build/update_stack_role.sh
+++ build/update_stack_role.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-aws cloudformation update-stack --profile=dev --stack-name BasicRole --template-body file://~/Temp/infrastructure/IAMrole.yaml \
- --parameters  ParameterKey=WebSourceBucket,ParameterValue=codedeploy.dev.kanzhang.me \
- ParameterKey=CodeDeployAppName,ParameterValue=csye6225-webapp \
- ParameterKey=GitHubActionIAMUser,ParameterValue=ghactions-app --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation update-stack --profile=demo --stack-name BasicRole \
+    --template-body file://./IAMrole.yaml \
+    --parameters  \
+    ParameterKey=WebSourceBucket,ParameterValue=codedeploy.prod.kanzhang.me \
+    ParameterKey=CodeDeployAppName,ParameterValue=csye6225-webapp \
+    ParameterKey=GitHubActionIAMUser,ParameterValue=ghactions-app \
+    --capabilities CAPABILITY_NAMED_IAM
